9c7e228e22425a9566e9b711c188a9ff952c81a5
Fernando Nascimento Oliveira <fernandocetep@outlook.com>
2021-11-12 10:50:07 -0300

    Instaladas todas as blibliotecas para inicio do app

---
+++ src/components/organisms/ToDoContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ToDoContainer = () => {
+  return (
+    <section>
+      <h1>Lista de Tarefas</h1>
+      <div>
+        <input type="text" placeholder="Digite sua tarefa" />
+        <button>Adicionar</button>
+      </div>
+    </section>
+  )
+}
+
+export default ToDoContainer

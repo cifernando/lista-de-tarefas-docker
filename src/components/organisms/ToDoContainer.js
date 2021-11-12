db8f7700f25c5d10a3182388cd20f7a7bca1b5ee
Fernando Nascimento Oliveira <fernandocetep@outlook.com>
2021-11-12 19:41:22 -0300

    Atualizada seçao de status da tarefa

--- src/components/organisms/ToDoContainer.js
+++ src/components/organisms/ToDoContainer.js
@@ -5,8 +5,10 @@ import ToDoTable from "../molecules/ToDoTable";
 const ToDoContainer = () => {
   return (
     <section className="w-4/6 h-4/6 flex flex-col gap-6  mt-10">
-      <div className="flex justify-center shadow sm:rounded-lg p-4 bg-white">
+      <div className="flex flex-col items-center shadow sm:rounded-lg p-4 bg-white">
         <h1 className="text-2xl font-bold text-gray-600">Lista de Tarefas</h1>
+        <p className="text-sm text-gray-400">Clique duas vezes para marcar a tarefa como feita.</p>
+        <p className="text-sm text-gray-400">Arraste e solte para mudar a prioridade da tarefa.</p>
       </div>
       <TaskForm />
       <ToDoTable />
